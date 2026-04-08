d583c1184474e4968dc6baa72d4c8fb417af9e39
Rodrigo Gregorio <rodrigo.gregorio1@hotmail.com>
2026-04-08 10:17:55 -0300

    config: gesture handler and reanimated

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import '@/styles/global.css';
